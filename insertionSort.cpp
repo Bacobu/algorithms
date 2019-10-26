9ed768a01bb23c522cecd155a389666ae2da06db
Vy <9575844+Bacobu@users.noreply.github.com>
2019-10-26 14:11:56 -0400

    Created stub for insertion sorts

---
+++ insertionSort.cpp
@@ -0,0 +1,18 @@
+/*
+File name: Insertion Sort
+Description: An implementation of a sorting algorithm that grabs one new value at a time to move to the sorted group on the left.
+Based upon: The Algorithm Design Manuel by Steven S. Skiena
+*/
+
+int main() {
+     //output something as a test
+     //call insertionSort and have it output something
+     //initialize and print an unsorted int array
+     //sort the array from above
+     //determine whether to sort other datatypes
+}
+
+int insertionSort() {
+
+     return 0;
+}
